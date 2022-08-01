a30f278105039b935a8fdc91415a751c68f6a825
BOTHP <110395203+BOTHP@users.noreply.github.com>
2022-08-01 17:36:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,19 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['94765294308']
+global.premium = ['94765294308']
+global.ownernomer = '94765294308'
+global.ownername = 'HIVIN_BOT'
+global.botname = 'HIVIN_BOT'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://chat.whatsapp.com/KyMsfH1KPAnKnQ78LjLGAz'
+global.packname = 'HIVIN_HP'
+
+global.author = 'HIVIN'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
